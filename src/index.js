f1277809b6287cfb2f8301e6f9d091b194c1f7fa
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-03-22 22:47:21 +0300

    addieng eventlisteer to the span

--- src/index.js
+++ src/index.js
@@ -1,1 +1,56 @@
 // Your code here
+
+document.addEventListener("DOMContentLoaded", getCharacter);
+
+function getCharacter() {
+  const characterName = document.querySelector("#character-bar");
+  //console.log(characterName); //testing
+
+  //fetching characters from json
+  fetch("http://localhost:3000/characters")
+    .then((response) => response.json())
+    .then((characters) => {
+      characters.forEach((character) => {
+        const span = document.createElement("span");
+        span.textContent = character.name;
+        //console.log(character); //testing
+
+        span.addEventListener("click", () => {
+          showCharacterDetails(character); //passing chaacter object
+        });
+
+        characterName.appendChild(span);
+      });
+    });
+}
+
+function showCharacterDetails(character) {
+  const detailedInfo = document.querySelector("#detailed-info");
+
+  detailedInfo.innerHTML = "";
+
+  const characterDetails = `
+     <img src="${character.image}" alt="${character.name}" />
+    <h2>${character.name}</h2>
+    <p>Votes: ${character.votes}</p>
+    <p>${character.description}</p>
+    `;
+
+  //add to Dom
+  detailedInfo.innerHTML = characterDetails;
+}
+
+const votesForm = document.querySelector("#votes-form");
+votesForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const votesInput = document.querySelector("#votes");
+  const newVotes = parseInt(votesInput.value);
+  const currentVotesElement = document.querySelector("#detailed-info ");
+
+  console.log(currentVotesElement); //testing
+  const currentVotes = parseInt(currentVotesElement.textContent.split(": ")[1]);
+  const totalVotes = currentVotes + newVotes;
+  currentVotesElement.textContent = `Votes: ${totalVotes}`;
+  votesInput.value = "";
+});
